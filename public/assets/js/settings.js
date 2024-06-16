79b4293fefcd11ed508df8f8cc829745209ac4e7
ONIxJUAN <Tyson.wilssens@outlook.com>
2024-06-16 17:45:28 +0200

    updated

---
+++ public/assets/js/settings.js
@@ -0,0 +1,16 @@
+function init() {
+  activeAvatar();
+}
+
+async function activeAvatar() {
+  let userStats = await fetch("/userStats");
+  userStats = await userStats.json();
+  // console.log(userStats);
+  document.getElementById("avatar").src = `/assets/images/${userStats.avatar}`;
+
+  let user = await fetch("/user");
+  user = await user.json();
+  document.getElementById("firstname").value = user.username;
+}
+
+init();
